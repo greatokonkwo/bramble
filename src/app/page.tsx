af75adc4a61d0e147d9af3829742291244b84420
Great Okonkwo <greatokonkwo@outlook.com>
2026-08-17 14:49:21 -0400

    fix: fix the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative min-h-svh w-full overflow-hidden bg-white px-6 py-8">
+    <main className="relative flex min-h-svh w-full flex-col overflow-hidden bg-[#FFFFFB] px-6 py-8">
       <header>
         <Image
           src="/bramble-logo.png"
@@ -14,16 +14,16 @@ export default function Home() {
         />
       </header>
 
-      <div className="flex min-h-[calc(100svh-8rem)] w-full flex-col items-center justify-center gap-6 md:gap-8">
-        <p className="w-full text-left text-4xl font-bold text-black sm:text-5xl md:w-3/4 md:text-6xl leading-none">
+      <div className="flex w-full flex-1 flex-col items-center justify-center gap-32 md:gap-68">
+        <p className="w-11/12 text-left text-2xl font-bold text-black sm:text-3xl md:w-3/5 md:text-4xl leading-none">
           your real life.
         </p>
-        <p className="w-full text-right text-4xl font-bold text-black sm:text-5xl md:w-3/4 md:text-6xl leading-none">
+        <p className="w-11/12 text-right text-2xl font-bold text-black sm:text-3xl md:w-3/5 md:text-4xl leading-none">
           infinitely playable.
         </p>
-
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwy-kbQPan1No85Ae3P4rOAW_zMApnvmyvnMVG2KsPeP-nrg/viewform?usp=header" className="mt-4 self-center rounded-full px-6 py-3 text-center text-xl font-bold text-black sm:text-2xl md:text-3xl">sign up for beta the waitlist</a>
       </div>
+
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwy-kbQPan1No85Ae3P4rOAW_zMApnvmyvnMVG2KsPeP-nrg/viewform?usp=header" className="mb-8 self-center rounded-full px-6 py-3 text-center text-xl font-bold text-black sm:text-2xl md:text-3xl">sign up for beta the waitlist</a>
     </main>
   );
 }
