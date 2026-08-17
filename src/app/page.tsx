40c1edee4b7e6557b1927b9966087d2988358ddc
Great Okonkwo <greatokonkwo@outlook.com>
2026-08-16 21:13:24 -0400

    feat: change the waitlist link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           infinitely playable.
         </p>
 
-        <a href="https://form.typeform.com/to/CV6jquJl" className="mt-4 self-center rounded-full px-6 py-3 text-center text-xl font-bold text-black sm:text-2xl md:text-3xl">sign up for beta the waitlist</a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwy-kbQPan1No85Ae3P4rOAW_zMApnvmyvnMVG2KsPeP-nrg/viewform?usp=header" className="mt-4 self-center rounded-full px-6 py-3 text-center text-xl font-bold text-black sm:text-2xl md:text-3xl">sign up for beta the waitlist</a>
       </div>
     </main>
   );
